5ea254620542415cb81dede53dfd74b9732014db
halitklyc <halitenes.kalayci@gmail.com>
2025-07-31 20:21:46 +0300

    model class

--- gpt/main.py
+++ gpt/main.py
@@ -31,3 +31,30 @@ def encode(sentence):
 #
 
 data = [encode(sentence) for sentence in sentences]
+
+class MiniGPT(nn.Module):
+    def __init__(self, vocab_size, d_model):
+        super().__init__()
+        self.embed = nn.Embedding(vocab_size, d_model)
+        self.pos_embed = nn.Parameter(torch.zeros(1, 3, d_model))
+        self.attn = nn.MultiheadAttention(embed_dim=d_model, num_heads=1, batch_first=True)
+        # Head -> Attentionda kaç farklı dikkat edilecek durum? (Odak noktası)
+        # özne-fiil
+        # sıfat-isim
+        # özne-özne bağı
+        self.ff = nn.Sequential(
+            nn.Linear(d_model, d_model),
+            nn.ReLU(),
+            nn.Linear(d_model, vocab_size)
+        )
+    def forward(self, x):
+        x = self.embed(x) + self.pos_embed[:, :x.size(1), :] #pozisyon kadar slice
+
+        # Query => Ne arıyorum? 
+        # Key => Hangi bilgilerin keylerini karşılaştıracağım? 
+        # Value => Sonuç olarak hangi bilgiyi çekeceğim?
+        attn_out, _ = self.attn(x, x, x)
+
+        out = self.ff(attn_out[:, -1, :])
+
+        return out
